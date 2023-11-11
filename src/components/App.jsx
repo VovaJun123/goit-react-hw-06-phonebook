2e1e2938fdc7c073e993e9453ebe2e034d947fd1
VovaJun123 <121401616+VovaJun123@users.noreply.github.com>
2023-11-11 15:29:43 +0000

    addd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,42 @@
-import { useSelector } from 'react-redux';
 
+
+
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { MyForm } from './Form/Form';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
+import { addContact, deleteContact } from '../redux/contactSlice';
+import { v4 as uuidv4 } from 'uuid';
 
 export const App = () => {
-  const users = useSelector(state => state.contacts.items);
+  const users = useSelector((state) => state.contacts.items);
+  const dispatch = useDispatch();
+
+  const onSubmitForm = ({ name, number }) => {
+   
+    const isUnique = !users.some((contact) => contact.name === name);
+
+    if (!isUnique) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    
+    dispatch(addContact({ id: uuidv4(), name, number }));
+  };
+
+  const onDeleteContactClick = (id) => {
+    dispatch(deleteContact(id));
+  };
+
+  
 
   return (
     <section>
       <div>
         <h1>Phonebook</h1>
-        <MyForm />
+        <MyForm onSubmitForm={onSubmitForm} />
       </div>
       <div>
         <h2>Contacts</h2>
@@ -21,7 +46,7 @@ export const App = () => {
           <>
             <h3>Your phonebook has {users.length} contacts</h3>
             <Filter />
-            <ContactList />
+            <ContactList onDeleteContactClick={onDeleteContactClick} />
           </>
         )}
       </div>
